95126bc5d001370ac219c0e74574b1233e401c73
stengvin <stengvin@mail.ru>
2026-08-16 20:24:33 +0300

    scene_state добавлен в глобалс автолоад, массив констант перенес туда

--- UI/Main_interface/mode_switch.gd
+++ UI/Main_interface/mode_switch.gd
@@ -2,29 +2,19 @@ extends Node2D
 
 @export_file("*.tscn") var thinking_scene_path: String
 
-enum State {
-	EXPLORATION,
-	THINKING,
-	NOTES
-}
-
-var current_state := State.EXPLORATION
-
 
 func _on_button_pressed() -> void:
-	current_state = (current_state + 1) % 3
+	SceneStateGlobal.current_state = (SceneStateGlobal.current_state + 1) % 3
 	$Pivo.rotation_degrees += 120
-	print(current_state)
-	match current_state:
-		State.THINKING:
+	print(SceneStateGlobal.current_state)
+	match SceneStateGlobal.current_state:
+		SceneStateGlobal.State.THINKING:
 			if thinking_scene_path.is_empty():
 				return
 			get_tree().change_scene_to_file(thinking_scene_path)
 
-		State.EXPLORATION:
-			# логика Exploration
+		SceneStateGlobal.State.EXPLORATION:
 			pass
 
-		State.NOTES:
-			# логика Notes
+		SceneStateGlobal.State.NOTES:
 			pass

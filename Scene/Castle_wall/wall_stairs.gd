96efd06244fb2ee527d16c01fc56696a605304fa
stengvin <stengvin@mail.ru>
2026-08-26 01:14:45 +0300

    стрелчка вниз со стен замка тож

--- Scene/Castle_wall/wall_stairs.gd
+++ Scene/Castle_wall/wall_stairs.gd
@@ -1,33 +1,41 @@
 extends Area2D
 
-@onready var wall_stairs_outline = $WallStairsOutline
-@export_file("*.tscn") var castle_interior_scene_path: String
 
+@onready var wall_stairs_outline = $WallStairsOutline
+
+@export_file("*.tscn") var castle_interior_scene_path: String
+@export var cursor_texture: Texture2D
 
 
 func _on_mouse_entered() -> void:
 	if castle_interior_scene_path.is_empty():
 		return
+
 	wall_stairs_outline.show()
+
+	if cursor_texture:
+		Input.set_custom_mouse_cursor(cursor_texture)
 
 
 func _on_mouse_exited() -> void:
 	if castle_interior_scene_path.is_empty():
 		return
+
 	wall_stairs_outline.hide()
+	Input.set_custom_mouse_cursor(null)
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	
+
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			interact()
 
 
 func interact() -> void:
-	
+
 	if castle_interior_scene_path.is_empty():
 		return
-	
+
+	Input.set_custom_mouse_cursor(null)
 	get_tree().change_scene_to_file(castle_interior_scene_path)
-	
